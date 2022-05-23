b24c1e1b8eb41058620ff14272d9828681c44816
HenRMJ <henryrmejia96@gmail.com>
2022-05-23 04:14:42 -0700

    Add dialogue scripts and text mesh pro
    I added text mesh pro into the game and imported everything that need. I also created a scriptable object that contains dialogue information. And I created the dialogue system script. Currently trying to figure out how I'm going to make it work

---
+++ Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogueSystem : MonoBehaviour
+{
+    [SerializeField] TMP_Text dialogueText;
+    [SerializeField] Dialogue dialogue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
