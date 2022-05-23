640c2d2d02b5a1b7283656ef2f799a0d0efec803
HenRMJ <henryrmejia96@gmail.com>
2022-05-23 02:54:33 -0700

    Add jumping
    I added a box collider to the player and a check to make sure it's on the ground so when you click jump you'll only jump if you're touching the ground. I also played with the physics a little so it feels a little nicer to move around. This should be working with controller and keyboard. I haven't tested with controller but keyboard is working.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,21 +7,34 @@ public class PlayerMovement : MonoBehaviour
 {
     // Adjust the move speed of the player with a slider
     [Range(0f, 10f)] [SerializeField] float moveSpeed;
+    [Range(0f, 10f)] [SerializeField] float jumpHeight;
 
     Rigidbody2D myBody;
     Vector2 moveInput;
+    BoxCollider2D groundCheck;
 
 
     // Start is called before the first frame update
     void Start()
     {
         myBody = GetComponent<Rigidbody2D>();
+        groundCheck = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (groundCheck.IsTouchingLayers())
+        {
+            Vector2 playerVelocity = new Vector2(myBody.velocity.x, myBody.velocity.y + jumpHeight);
+
+            myBody.velocity = playerVelocity;
+        }
     }
 
     // OnMove gets called by Input System whenever a value is detected
