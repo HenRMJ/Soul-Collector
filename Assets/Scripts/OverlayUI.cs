7f955d03174f1e6c0210bfe70afb5dc641bb67de
HenRMJ <henryrmejia96@gmail.com>
2022-05-28 11:50:21 -0700

    Complete overlay UI overhaul
    I changed the way that the overlay UI works slightly. But I also created art for the UI and now we have a nice looking Overlay UI.

--- Assets/Scripts/OverlayUI.cs
+++ Assets/Scripts/OverlayUI.cs
@@ -6,19 +6,40 @@ using UnityEngine.UI;
 
 public class OverlayUI : MonoBehaviour
 {
+    [Header("Item Panels")]
     [SerializeField] Image itemOnePanel;
     [SerializeField] Image itemTwoPanel;
     [SerializeField] Image itemThreePanel;
     [SerializeField] Image itemFourPanel;
 
+    [Header("Item Number Trackers")]
     [SerializeField] TMP_Text itemOneTracker;
     [SerializeField] TMP_Text itemTwoTracker;
     [SerializeField] TMP_Text itemThreeTracker;
     [SerializeField] TMP_Text itemFourTracker;
 
+    [Header("Colors")]
     [SerializeField] Color inactive;
     [SerializeField] Color active;
-    [SerializeField] Color selected;
+
+    [Header("Not Selected Images")]
+    [SerializeField] Sprite itemOneNotSelected;
+    [SerializeField] Sprite itemTwoNotSelected;
+    [SerializeField] Sprite itemThreeNotSelected;
+    [SerializeField] Sprite itemFourNotSelected;
+
+    [Header("Selected Images")]
+    [SerializeField] Sprite itemOneSelected;
+    [SerializeField] Sprite itemTwoSelected;
+    [SerializeField] Sprite itemThreeSelected;
+    [SerializeField] Sprite itemFourSelected;
+
+    [Header("Tracker Backgrounds")]
+    [SerializeField] GameObject oneBackground;
+    [SerializeField] GameObject twoBackground;
+    [SerializeField] GameObject threeBackground;
+    [SerializeField] GameObject fourBackground;
+
 
     string selectedItem;
     Consumable consumables;
@@ -53,44 +74,57 @@ public class OverlayUI : MonoBehaviour
         checkThree = consumables.GetThreeCount();
         checkFour = consumables.GetFourCount();
 
+        itemOnePanel.sprite = itemOneNotSelected;
+        itemTwoPanel.sprite = itemTwoNotSelected;
+        itemThreePanel.sprite = itemThreeNotSelected;
+        itemFourPanel.sprite = itemFourNotSelected;
+
         if (checkOne > 0)
         {
             itemOneTracker.text = checkOne.ToString();
             itemOnePanel.color = active;
+            oneBackground.SetActive(true);
         } else
         {
             itemOneTracker.text = string.Empty;
             itemOnePanel.color = inactive;
+            oneBackground.SetActive(false);
         }
 
         if (checkTwo > 0)
         {
             itemTwoTracker.text = checkTwo.ToString();
             itemTwoPanel.color = active;
+            twoBackground.SetActive(true);
         } else
         {
             itemTwoTracker.text = string.Empty;
             itemTwoPanel.color = inactive;
+            twoBackground.SetActive(false);
         }
 
         if (checkThree > 0)
         {
             itemThreeTracker.text = checkThree.ToString();
             itemThreePanel.color = active;
+            threeBackground.SetActive(true);
         } else
         {
             itemThreeTracker.text = string.Empty;
             itemThreePanel.color = inactive;
+            threeBackground.SetActive(false);
         }
 
         if (checkFour > 0)
         {
             itemFourTracker.text = checkFour.ToString();
             itemFourPanel.color = active;
+            fourBackground.SetActive(true);
         } else
         {
             itemFourTracker.text = string.Empty;
             itemFourPanel.color = inactive;
+            fourBackground.SetActive(false);
         }
     }
 
@@ -102,16 +136,16 @@ public class OverlayUI : MonoBehaviour
 
         if (selectedItem == _ONE && checkOne != 0)
         {
-            itemOnePanel.color = selected;
+            itemOnePanel.sprite = itemOneSelected;
         } else if (selectedItem == _TWO && checkTwo != 0)
         {
-            itemTwoPanel.color = selected;
+            itemTwoPanel.sprite = itemTwoSelected;
         } else if (selectedItem == _THREE && checkThree != 0)
         {
-            itemThreePanel.color = selected;
+            itemThreePanel.sprite = itemThreeSelected;
         } else if (selectedItem == _FOUR && checkFour != 0) 
         {
-            itemFourPanel.color = selected;
+            itemFourPanel.sprite = itemFourSelected;
         }
     }
 }
