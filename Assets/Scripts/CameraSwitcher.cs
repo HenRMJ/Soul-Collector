9f130ee65d66cf72d5c82618ee936ed5cc2e6c6a
HenRMJ <henryrmejia96@gmail.com>
2022-06-06 04:44:52 -0700

    Update Cameras
    I added virtual cameras to all the levels and changed the flame prefab to include a virtual camera. Now when the player goes inside the range of the flame. It will zoom into the NPC

--- Assets/Scripts/CameraSwitcher.cs
+++ Assets/Scripts/CameraSwitcher.cs
@@ -6,35 +6,22 @@ using Cinemachine;
 public class CameraSwitcher : MonoBehaviour
 {
     [SerializeField] CinemachineVirtualCamera follow;
-    [SerializeField] CinemachineVirtualCamera[] dialogue;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("flame1"))
+        if (collision.CompareTag("flame"))
         {
             follow.Priority = 0;
-            dialogue[0].Priority = 1;
-        }
-
-        if (collision.CompareTag("flame2"))
-        {
-            follow.Priority = 0;
-            dialogue[1].Priority = 1;
+            collision.gameObject.GetComponentsInChildren<CinemachineVirtualCamera>()[0].Priority = 1;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("flame1"))
+        if (collision.CompareTag("flame"))
         {
             follow.Priority = 1;
-            dialogue[0].Priority = 0;
-        }
-
-        if (collision.CompareTag("flame2"))
-        {
-            follow.Priority = 1;
-            dialogue[1].Priority = 0;
+            collision.gameObject.GetComponentsInChildren<CinemachineVirtualCamera>()[0].Priority = 0;
         }
     }
 }
