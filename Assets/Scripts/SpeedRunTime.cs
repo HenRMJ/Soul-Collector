1bde2b5cecee88f5f5850752facf6f3851b9774a
HenRMJ <henryrmejia96@gmail.com>
2022-06-24 05:18:11 -0700

    Add speedrun timer
    I added a speed run timer with a high score system. So now the fastest time you get should display at the end of the game.

---
+++ Assets/Scripts/SpeedRunTime.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SpeedRunTime : MonoBehaviour
+{
+    [SerializeField] TMP_Text timerText;
+    [SerializeField] TMP_Text fastestTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerText.text == PlayerPrefs.GetString("Run")) { return; }
+
+        if (!(PlayerPrefs.HasKey("Fastest_Time")) || PlayerPrefs.GetFloat("Fastest_Time") == 0f)
+        {
+            PlayerPrefs.SetFloat("Fastest_Time", PlayerPrefs.GetFloat("Speed"));
+            PlayerPrefs.SetString("HighSpeed", PlayerPrefs.GetString("Run"));
+        }
+
+        if (PlayerPrefs.GetFloat("Speed") < PlayerPrefs.GetFloat("Fastest_Time"))
+        {
+            PlayerPrefs.SetFloat("Fastest_Time", PlayerPrefs.GetFloat("Speed"));
+            PlayerPrefs.SetString("HighSpeed", PlayerPrefs.GetString("Run"));
+        }
+
+        fastestTime.text = PlayerPrefs.GetString("HighSpeed");
+        timerText.text = PlayerPrefs.GetString("Run");
+        Debug.Log("Set run time");
+        Debug.Log(PlayerPrefs.GetFloat("Fastest_Time"));
+        Debug.Log(PlayerPrefs.GetFloat("Speed"));
+    }
+}
--- Assets/Scripts/SpeedTimer.cs
+++ Assets/Scripts/SpeedTimer.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System;
+using UnityEngine.SceneManagement;
 
 public class SpeedTimer : MonoBehaviour
 {
     [SerializeField] TMP_Text timerText;
 
     double timer;
+    string runTime;
 
     private void Awake()
     {
@@ -24,13 +26,41 @@ public class SpeedTimer : MonoBehaviour
         }
     }
 
+    private void OnLevelWasLoaded()
+    {
+        string currentScene = SceneManager.GetActiveScene().name;
+
+        if (currentScene.Contains("Level"))
+        {
+            return;
+        } else
+        {
+            PlayerPrefs.SetString("Run", runTime);
+            PlayerPrefs.SetFloat("Speed", Convert.ToSingle(timer));
+            Destroy(gameObject);
+        }
+    }
+
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         timer += Time.deltaTime;
-        float time = Convert.ToSingle(Math.Round(timer, 2));
 
+        double minute = Math.Floor(timer / 60);
+        double seconds = Math.Round(timer % 60, 3);
 
-        timerText.text = time.ToString();
+        string secondsAsString;
+
+        if (seconds.ToString().IndexOf(".") == 1)
+        {
+            secondsAsString = seconds.ToString().Insert(0, "0");
+        } else
+        {
+            secondsAsString = seconds.ToString();
+        }
+
+        runTime = minute.ToString() + ":" + secondsAsString.Replace(".", ":");
+
+        timerText.text = runTime;
     }
 }
